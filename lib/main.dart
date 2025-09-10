5e942538a91c3c7799416bc737a4037cb6fcc9b6
RudiAhmad <rudi.ahmad31@gmail.com>
2025-09-10 23:38:40 +0700

    testing kalkulator

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'menu/kalkulator.dart'; // langsung import kalkulator
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Testing Kalkulator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: KalkulatorPage(), // langsung buka kalkulator
+    );
+  }
+}
